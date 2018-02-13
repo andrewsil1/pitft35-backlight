c864a2a5a3be3a1071c161d4d0fa8b8bb7d063e9
Andy Silverman <andrewsi@outlook.com>
2018-02-12 18:38:28 -0800

    Updates to syslogging, fix fencepost logic.

--- PiTftDpms.c
+++ PiTftDpms.c
@@ -18,11 +18,18 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <syslog.h>
 
 /*  Global variables  */
 
 Display *     display;
 static char * appname;
+
+void logmsg(int level, char * msg) {
+	openlog(appname, LOG_ODELAY, LOG_USER);
+	syslog(level, msg);
+	closelog();
+}
 
 /*  main() function  */
 
@@ -31,15 +38,21 @@ int main( int argc, char * argv[] ) {
     /*  Display variables  */
 
     char *       display_name = NULL;
+    char * 	 msg = NULL; 
+
+    msg = malloc(sizeof(char) * 100);
  
     appname = argv[0];
 
+    snprintf(msg, 100, "Starting PiTftDpms display backlight utility.\n");
+    logmsg(LOG_INFO,msg);
+
     /*  Connect to X server  */
 
-    if ( (display = XOpenDisplay(display_name)) == NULL ) {
-	fprintf(stderr, "%s: couldn't connect to X server %s\n",
-		appname, display_name);
-	exit(EXIT_FAILURE);
+    if ((display = XOpenDisplay(display_name)) == NULL ) {
+        snprintf(msg, 100, "Couldn't connect to X server %s\n", display_name);
+        logmsg(LOG_ERR,msg);
+        exit(EXIT_FAILURE);
     }
 
 int dpmsEvent;
@@ -47,7 +60,8 @@ int dpmsError;
 
     /* Make sure we can talk to the DPMS X extension */
     if (!DPMSQueryExtension(display, &dpmsEvent, &dpmsError)) {
-        fprintf(stderr,"%s: X claims it's doesn't support the DPMS extension!", appname);
+        snprintf(msg, 100, "X claims it doesn't support the DPMS extension!\n", display_name);
+        logmsg(LOG_ERR,msg);
         exit(EXIT_FAILURE);
     }
     
@@ -59,31 +73,38 @@ int dpmsError;
     
     /* Initialize our current understanding of the display power state. */
     if (!DPMSInfo(display, &power_level, &state)) {
+      snprintf(msg, 100, "Unable to read the display power state, exiting.\n");
+      logmsg(LOG_ERR,msg);
       exit(EXIT_FAILURE);
     }
     old_power_level = power_level;
         
     while ( 1 ) {
         
-        if (!DPMSInfo(display, &power_level, &state)) {
-          exit(EXIT_FAILURE);
-        } else {
-            /*  Only update power state if X11 DPMS extension indicates that there has been a change in state. */
-            if (power_level != old_power_level) {
-                old_power_level = power_level;
-                int fd = open("/sys/class/backlight/soc:backlight/brightness", O_WRONLY);
-                if (fd == -1) {
-                    fprintf(stderr, "Couldn't open backlight device file\n", appname);
-                    exit(EXIT_FAILURE);
-                }
-                if (power_level != DPMSModeOn)
-                    write(fd, "0\n", 2);
-                else
-                    write(fd, "1\n", 2);
-                close(fd);
+        /*  Only update power state if X11 DPMS extension indicates that there has been a change in state. */
+        if (power_level != old_power_level) {
+            old_power_level = power_level;
+            int fd = open("/sys/class/backlight/soc:backlight/brightness", O_WRONLY);
+            if (fd == -1) {
+                snprintf(msg, 100, "Couldn't open backlight device file. Need sudo?\n");
+                logmsg(LOG_ERR,msg);
+                exit(EXIT_FAILURE);
             }
+            if (power_level != DPMSModeOn)
+                write(fd, "0\n", 2);
+            else
+                write(fd, "1\n", 2);
+            close(fd);
         }
+    
         usleep(250000); /* Check every 0.25 seconds. */
-    }
-    return EXIT_SUCCESS;   /*  We shouldn't get here  */
+        
+        /* Update the power state after sleeping. */
+        if (!DPMSInfo(display, &power_level, &state)) {
+            snprintf(msg, 100, "Unable to read the display power state, exiting.\n");
+            logmsg(LOG_ERR,msg);
+            exit(EXIT_FAILURE);
+        }
+    } /* End While */
+    return EXIT_SUCCESS;   /*  We shouldn't ever get here  */
 }
